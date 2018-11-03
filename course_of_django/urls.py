73e66e083f0ee7fbf8f0a7e0d838cc58c359cb79
Alehand125 <Alehand125@gmail.com>
2018-11-03 15:40:54 +0600

    init git

---
+++ course_of_django/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from test1 import views
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'course_of_django.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.hello),
+]
+
---
+++ test1/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponce
+
+# Create your views here.
+def hello(requiest):
+  
+
+
+
+  return HttpResponce("hello python")
+  
